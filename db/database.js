03ffa1049d8c5d0651d45e8043c70e774dcc92a5
unknown <juni.93.juni@gmail.com>
2020-10-05 15:04:49 +0200

    db bugfix

--- db/database.js
+++ db/database.js
@@ -1,6 +1,6 @@
 const sqlite3 = require('sqlite3').verbose()
 
-let db = new sqlite3.Database(process.env.LOCAL_SQLITE, (err) => {
+const db = new sqlite3.Database(process.env.LOCAL_SQLITE, (err) => {
     if(err){
         console.error(err.message)
         throw err
